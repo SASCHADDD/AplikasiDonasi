0ca9a9b56304f43942d2719777d5f7a9aee46770
SASCHADDD <saschadanu23@gmail.com>
2026-01-14 01:28:53 +0700

    halaman login

---
+++ app/src/main/java/com/example/aplikasidonasi/view/auth/HalamanLogin.kt
@@ -0,0 +1,79 @@
+package com.example.aplikasidonasi.view.auth
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.aplikasidonasi.model.User
+import com.example.aplikasidonasi.viewmodel.AuthViewModel
+
+@Composable
+fun HalamanLogin(
+    authViewModel: AuthViewModel,
+    onLoginSuccess: (User) -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val user by authViewModel.user.observeAsState()
+    val error by authViewModel.error.observeAsState()
+
+    // Jika login berhasil
+    LaunchedEffect(user) {
+        user?.let {
+            onLoginSuccess(it)
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text("Login")
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") }
+        )
+
+        Button(onClick = {
+            authViewModel.login(email, password)
+        }) {
+            Text("Login")
+        }
+
+        error?.let {
+            Text(
+                text = it,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+    }
+}
