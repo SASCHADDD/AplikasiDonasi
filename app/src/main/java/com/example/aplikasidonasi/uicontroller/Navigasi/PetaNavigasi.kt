29c00a8ad02c8387ebc6b1294ba2a42167fef5fe
SASCHADDD <saschadanu23@gmail.com>
2026-01-14 02:34:20 +0700

    membuat object untuk navigasi

---
+++ app/src/main/java/com/example/aplikasidonasi/uicontroller/Navigasi/PetaNavigasi.kt
@@ -0,0 +1,8 @@
+package com.example.aplikasidonasi.uicontroller.Navigasi
+
+object PetaNavigasi {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME = "home"
+
+}
